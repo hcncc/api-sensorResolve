0e373c72fd8e2685288c2bd5186fdb26447b831f
hcncc <sbombastico@gmail.com>
2024-06-06 13:20:45 +0100

    feat: Delete host in other position

--- back/src/server.ts
+++ back/src/server.ts
@@ -200,7 +200,6 @@ app.get("/sensor/:enterpriseId", async(request, reply)=>{
 })
 
 app.listen({ 
-    host: '0.0.0.0', 
     port: process.env.PORT ? Number(process.env.PORT) : 3333, 
 })
     .then(() => console.log(`Server SensorResolve started in port: ${process.env.PORT ?? 3333}`))
